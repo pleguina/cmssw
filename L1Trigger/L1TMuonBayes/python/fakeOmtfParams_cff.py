3a2b29d8e51eb8507fc9083d9e7d60503c6bd26a
Karol Bunkowski <karol.bunkowski@cern.ch>
2020-04-15 11:56:52 +0200

    using algo version="0x0004"
    goldenPatternResultFinalizeFunction = cms.int32(0)
    nn v32

--- L1Trigger/L1TMuonBayes/python/fakeOmtfParams_cff.py
+++ L1Trigger/L1TMuonBayes/python/fakeOmtfParams_cff.py
@@ -15,7 +15,6 @@ omtfParams = cms.ESProducer(
         #cms.PSet(patternsXMLFile = cms.FileInPath("L1Trigger/L1TMuonBayes/test/expert/Patterns_0x0003_TT.xml")),
     ),
     configXMLFile = cms.FileInPath("L1Trigger/L1TMuon/data/omtf_config/hwToLogicLayer_0x0006.xml"),
-
 )
 
 
