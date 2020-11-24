7e98e1530d700acd49748e9aba2232a392d59a52
Karol Bunkowski <karol.bunkowski@cern.ch>
2020-11-24 10:09:37 +0100

    t91 - in GoldenPatternResult::finalise9() no hit penatly is -16
    ~1% better efficiency for high pt than in the t90, for the low pt also
    little higher, but still lower then in the t82

--- L1Trigger/L1TMuonOverlapPhase1/src/Omtf/GoldenPatternResult.cc
+++ L1Trigger/L1TMuonOverlapPhase1/src/Omtf/GoldenPatternResult.cc
@@ -305,13 +305,13 @@ void GoldenPatternResult::finalise9() {
         }
       } else {
         if (stubResults[iLogicLayer].getPdfVal() == 0)  //banding layer fired, but not fits to the pdf, N>B works only with the patterns having "no hit value" and with noHitValueInPdf = True
-          pdfSum -= 32;
+          pdfSum -= 16;
         else
           pdfSum += stubResults[iLogicLayer].getPdfVal();  //banding layer not fired at all
       }
     } else {
       if (iLogicLayer < 10 && stubResults[iLogicLayer].getPdfVal() == 0)
-        pdfSum -= 64;
+        pdfSum -= 16;
       else
         pdfSum += stubResults[iLogicLayer].getPdfVal();
       if (firedLayerBits & (1 << iLogicLayer)) {  //pdfSum is counted always
