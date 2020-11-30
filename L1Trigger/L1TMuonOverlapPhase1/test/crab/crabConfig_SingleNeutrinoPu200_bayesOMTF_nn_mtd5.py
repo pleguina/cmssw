4c272d2087bd74d8980a1e2d83477a24778823f1
Karol Bunkowski <karol.bunkowski@cern.ch>
2020-11-30 17:33:42 +0100

    t100 - running more jobs

--- L1Trigger/L1TMuonOverlapPhase1/test/crab/crabConfig_SingleNeutrinoPu200_bayesOMTF_nn_mtd5.py
+++ L1Trigger/L1TMuonOverlapPhase1/test/crab/crabConfig_SingleNeutrinoPu200_bayesOMTF_nn_mtd5.py
@@ -1,14 +1,14 @@
-from CRABClient.UserUtilities import config, getUsernameFromSiteDB
+from CRABClient.UserUtilities import config #, getUsernameFromSiteDB
 config = config()
 
-config.General.requestName = 'omtf_nn_MC_analysis_SingleNeutrino_PU200_mtd5_v2_t47'
+config.General.requestName = 'omtf_nn_MC_analysis_SingleNeutrino_PU200_mtd5_v3_t100'
 #config.General.workArea = 'crab_projects'
 config.General.transferOutputs = True
 config.General.transferLogs = True
 
 config.JobType.pluginName = 'Analysis'
 #config.JobType.psetName = 'runMuonOverlapTTMergerAnalyzerCrab.py'
-config.JobType.psetName = 'runMuonOverlap_nn_mtd5.py'
+config.JobType.psetName = 'runMuonOverlap_nn_phase2.py'
 config.JobType.pyCfgParams = ['rate']
 
 config.Data.inputDataset = '/NeutrinoGun_E_10GeV/PhaseIIMTDTDRAutumn18DR-PU200_103X_upgrade2023_realistic_v2-v1/FEVT'
@@ -22,14 +22,15 @@ config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = 50
 #config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
 config.Data.publication = False
-config.Data.outputDatasetTag = 'CRAB3_omtf_nn_MC_analysis_SingleNeutrino_PU200_mtd5_v2_t47'
+config.Data.outputDatasetTag = 'CRAB3_omtf_nn_MC_analysis_SingleNeutrino_PU200_mtd5_v3_t100'
 config.Data.totalUnits = 2599
 config.Data.ignoreLocality = False
 
 config.section_("Debug")
 config.Debug.extraJDL = ['+CMS_ALLOW_OVERFLOW=False']
 
-config.Site.storageSite = 'T2_PL_Swierk'
+#config.Site.storageSite = 'T2_PL_Swierk'
+config.Site.storageSite = 'T2_CH_CERN'
 #config.Site.whitelist = ['T2_US_Caltech', 'T3_US_Colorado']
 #config.Site.blacklist = ['T2_US_Purdue', 'T2_US_Florida', 'T2_US_MIT', 'T2_US_Nebraska', 'T2_US_Purdue', 'T2_US_UCSD', 'T2_US_Vanderbilt', 'T2_US_Wisconsin']
 #does not work, so no matter
