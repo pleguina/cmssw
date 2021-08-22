1fe4e8ace7b8f4b19cff32abca70e9f2081824ce
Karol Bunkowski <karol.bunkowski@cern.ch>
2021-08-22 12:33:17 +0200

    L1TMuonOverlapPhase1TrackProducer inherits from  edm::one::EDProducer
    with edm::one::WatchRuns customization

--- L1Trigger/L1TMuonOverlapPhase1/plugins/L1TMuonOverlapPhase1TrackProducer.h
+++ L1Trigger/L1TMuonOverlapPhase1/plugins/L1TMuonOverlapPhase1TrackProducer.h
@@ -12,14 +12,20 @@
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/EventSetup.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
-#include "FWCore/Framework/interface/EDProducer.h"
+#include "FWCore/Framework/interface/one/EDProducer.h"
 
 #include "DataFormats/L1DTTrackFinder/interface/L1MuDTChambPhContainer.h"
 #include "DataFormats/L1DTTrackFinder/interface/L1MuDTChambThContainer.h"
 #include "DataFormats/CSCDigi/interface/CSCCorrelatedLCTDigiCollection.h"
 #include "DataFormats/RPCDigi/interface/RPCDigiCollection.h"
 
-class L1TMuonOverlapPhase1TrackProducer : public edm::EDProducer {
+/**
+ * The OMTF emulator cannot be run in multiple threads, because GoldenPatternBase keeps GoldenPatternResult's
+ * which are then used in the GoldenPatternBase::finalise called at the end of OMTFProcessor<GoldenPatternType>::processInput.
+ * Also patterns generation relies on updating the statistics for the patterns
+ * This can be fixed, but requires some work.
+ */
+class L1TMuonOverlapPhase1TrackProducer : public edm::one::EDProducer<edm::one::WatchRuns> {
 public:
   L1TMuonOverlapPhase1TrackProducer(const edm::ParameterSet&);
 
@@ -30,6 +36,8 @@ public:
   void endJob() override;
 
   void beginRun(edm::Run const& run, edm::EventSetup const& iSetup) override;
+
+  virtual void endRun(edm::Run const& run, edm::EventSetup const& iSetup) override {};
 
   void produce(edm::Event&, const edm::EventSetup&) override;
 
