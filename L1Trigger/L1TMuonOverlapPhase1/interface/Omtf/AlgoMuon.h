3fe07c17335dffe96a968da2863c7376dd10bdc3
Karol Bunkowski <karol.bunkowski@cern.ch>
2021-09-27 19:26:56 +0200

    setting HwPtUnconstrained in output RegionalMuonCand
    added AlgoMuon::getPtUnconstrained()
    goldenPaternUpt cannot be const

--- L1Trigger/L1TMuonOverlapPhase1/interface/Omtf/AlgoMuon.h
+++ L1Trigger/L1TMuonOverlapPhase1/interface/Omtf/AlgoMuon.h
@@ -38,6 +38,12 @@ public:
     return goldenPatern->key().thePt;
   }
 
+  int getPtUnconstrained() const {
+    if (goldenPaternUpt == nullptr)
+      return -1;
+    return goldenPaternUpt->key().thePt;
+  }
+
   int getCharge() const {
     if (goldenPatern == nullptr)
       return 0;
@@ -94,7 +100,7 @@ public:
     return goldenPaternUpt;
   }
 
-  void setGoldenPaternUpt(const GoldenPatternBase* goldenPaternUpt) {
+  void setGoldenPaternUpt(GoldenPatternBase* goldenPaternUpt) {
     this->goldenPaternUpt = goldenPaternUpt;
   }
 
@@ -125,7 +131,7 @@ private:
   GoldenPatternBase* goldenPatern = nullptr;
 
   //GoldenPattern without vertex constraint (unconstrained pt)
-  const GoldenPatternBase* goldenPaternUpt = nullptr;
+  GoldenPatternBase* goldenPaternUpt = nullptr;
 
   //int m_q = -1;
   int m_bx = 0;
