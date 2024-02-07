2c4ddbac5fcba7a63f3aac5e16ac526417281b58
pleguina <p.leguina.lopez@gitlab.com>
2024-02-07 12:03:11 +0100

    sdsdsdsdsd

--- CondFormats/DataRecord/interface/L1TMuonEndcapParamsRcd.h
+++ CondFormats/DataRecord/interface/L1TMuonEndcapParamsRcd.h
@@ -1,21 +1,19 @@
-#ifndef L1TEMTFParamsRcd_L1TEMTFParamsRcd_h
-#define L1TEMTFParamsRcd_L1TEMTFParamsRcd_h
-// -*- C++ -*-
+// L1TMuonEndCapParamsRcd                                                                                            
+// Description: Record for L1TMuonEndCapParams
 //
-// Class  :     L1TMuonEndcapParamsRcd
+// automatically generate by make_records.pl
 //
-// Author:      Matthew Carver
-// Created:
-//
-
-//#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
-//#include "CondFormats/DataRecord/interface/L1TriggerKeyListExtRcd.h"
-//#include "CondFormats/DataRecord/interface/L1TriggerKeyExtRcd.h"
-
-//class L1TMuonEndcapParamsRcd : public edm::eventsetup::DependentRecordImplementation<L1TMuonEndcapParamsRcd, boost::mpl::vector<L1TriggerKeyListExtRcd,L1TriggerKeyExtRcd> > {};
+#ifndef CondFormatsDataRecord_L1TMuonEndCapParams_h
+#define CondFormatsDataRecord_L1TMuonEndCapParams_h
 
 #include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
 
-class L1TMuonEndcapParamsRcd : public edm::eventsetup::EventSetupRecordImplementation<L1TMuonEndcapParamsRcd> {};
+class L1TMuonEndCapParamsRcd : public edm::eventsetup::EventSetupRecordImplementation<L1TMuonEndCapParamsRcd> {};
+
+// Dependent record implmentation:
+//#include "FWCore/Framework/interface/DependentRecordImplementation.h"
+//#include "CondFormats/DataRecord/interface/L1TriggerKeyListRcd.h"
+//#include "CondFormats/DataRecord/interface/L1TriggerKeyRcd.h"
+//class L1TMuonEndCapParamsRcd : public edm::eventsetup::DependentRecordImplementation<L1TMuonEndCapParamsRcd, boost::mpl::vector<L1TriggerKeyListRcd,L1TriggerKeyRcd> > {};
 
 #endif
